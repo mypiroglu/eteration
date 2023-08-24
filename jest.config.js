140fde8f277c37f9aff710b31e6a21c7ccf9e728
mypiroglu <muhammetyusufpiroglu@gmail.com>
2023-08-24 17:49:34 +0300

    test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  preset: 'react-native',
-  transform: {
-    '^.+\\.js$': 'babel-jest',
+  preset: 'jest-react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation|@react-native)',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFiles: ['./jest.setup.js'],
+  moduleNameMapper: {
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '^@react-native-community/async-storage':
+      '@react-native-community/async-storage/jest/async-storage-mock',
   },
-  transformIgnorePatterns: ['/node_modules/'],
 };
